d6904f8907d02baa9d783a7c8eddaa3c9a76c79e
NoScrying <42573630+NoScrying@users.noreply.github.com>
2024-08-13 18:52:19 +0200

    Update RUN.lua

--- xivhotbar/data/Noscrying/RUN.lua
+++ xivhotbar/data/Noscrying/RUN.lua
@@ -2,18 +2,18 @@
 xivhotbar_keybinds_job['Base'] = {
  -- Hotbar #1
 	{'b 1 4', 'ma', 'Foil', 'me', 'Foil', 'Angel'},
-	{'b 1 5', 'ja', 'Embolden', 'me', 'Enhance+', 'TBN'},
-	{'b 1 6', 'ma', 'Flash', 'stnpc', 'Flash'},
-  	{'b 1 9', 'ma', 'Phalanx', 'me', 'Phalanx'},
+	{'b 1 5', 'ja', 'Embolden', 'me', 'Enhc+', 'TBN'},
+	{'b 1 6', 'ma', 'Flash', 'stnpc', 'Flash', "Flash"},
+  	{'b 1 9', 'ma', 'Phalanx', 'me', 'Phalnx', "Phalanx"},
 	{'b 1 10', 'ja', 'Vivacious Pulse', 'me', 'Healing', 'Heal'},
  -- Hotbar #2
 	{'b 4 1', 'ja', 'Vallation', 'me', 'Val.', 'Vallation'},
-	{'b 4 2', 'ja', 'Valiance', 'me', 'Valiance', 'Valiance'},
+	{'b 4 2', 'ja', 'Valiance', 'me', 'Val+', 'Valiance'},
 	{'b 4 3', 'ja', 'Pflug', 'me', 'Pflug', 'Oblation'},
 	{'b 4 4', 'ja', 'Battuta', 'me', 'Parry+', 'Battuta'},
-	{'b 4 6', 'ma', 'Refresh', 'stpc', 'Refresh'},
+	{'b 4 6', 'ma', 'Refresh', 'stpc', 'Refrsh'},
 
-	{'b 4 5', 'ma', 'Regen IV', 'stpc', 'Regen'},
+	{'b 4 5', 'ma', 'Regen IV', 'stpc', 'Regn'},
 	{'b 4 7', 'ma', 'Temper', 'me', 'Tri-ATK'},
 	{'b 4 8', 'ct', 'ra', 't', 'RA', 'Ranged'},
 	{'b 3 9', 'ja', 'One for All', 'me', 'OfA', 'OFA'},
@@ -39,7 +39,7 @@ xivhotbar_keybinds_job['Base'] = {
 	{'b 4 9', 'ja', 'Liement', 'me', 'Liement', 'Liement'},
 	---
 	{'f 1 5', 'ma', 'Foil', 'me', 'Foil', 'Angel'},
-	{'f 4 1', 'ma', 'Protect IV', 'stpc', 'Protect'},
+	{'f 4 1', 'ma', 'Protect IV', 'stpc', 'Prtct'},
 	{'f 4 2', 'ma', 'Shell V', 'stpc', 'Shell'},
 
 	{'f 3 1', 'ma', 'Barblizzard', 'me', 'Ice'},
@@ -49,12 +49,12 @@ xivhotbar_keybinds_job['Base'] = {
 	{'f 3 5', 'ma', 'Barwater', 'me', 'Water'},
 	{'f 3 3', 'ma', 'Barfire', 'me', 'Fire'},
 	
-	{'f 2 4', 'ma', 'Barsleep', 'me', 'B.Sleep'},
+	{'f 2 4', 'ma', 'Barsleep', 'me', 'B.Slp'},
 	{'f 2 1', 'ma', 'Barparalyze', 'me', 'B.Para'},
 	{'f 2 2', 'ma', 'Barsilence', 'me', 'B.Sil'},
-	{'f 2 3', 'ma', 'Baramnesia', 'me', 'B.Amnes'},
+	{'f 2 3', 'ma', 'Baramnesia', 'me', 'B.Amns'},
 	
-	{'f 4 4', 'ma', 'Stoneskin', 'me', 'Stoneskin'},
+	{'f 4 4', 'ma', 'Stoneskin', 'me', 'StnSkn'},
 	{'f 4 5', 'ma', 'Aquaveil', 'me', 'Veil'},	
    {'f 4 10', 'ja', 'Elemental Sforzo', 'me', 'Sforzo'},
     {'f 4 9', 'ja', 'Odyllic Subterfuge', 'stnpc', 'Odyllic'},
@@ -65,25 +65,33 @@ xivhotbar_keybinds_job['BLM'] = {
 }
 xivhotbar_keybinds_job['WAR'] = {
     {'b 1 3',  'ja', 'Provoke', 'stnpc', 'Prov.'},
-    {'b 2 3',  'ja', 'Berserk', 'me', 'Berserk', 'Berserk'},
+    {'b 2 3',  'ja', 'Berserk', 'me', 'Brsrk', 'Berserk'},
     {'b 2 4',  'ja', 'Warcry', 'me', 'War.', 'Warcry'},
 	{'b 2 5', 'ja', 'Aggressor', 'me', 'Aggr.', 'Agressor'}
 }
+xivhotbar_keybinds_job['PLD'] = {
+    {'b 1 7',  'ma', 'Cure IV', 'stpc', 'CureIV'},
+    {'b 1 8',  'ma', 'Banishga', 't', 'Bnshga', "Angel"},
+    {'b 2 7',  'ja', 'Sentinel', 'me', 'Sntnl', 'Sentinel'},
+    {'b 2 8',  'ja', 'Cover', 'stpc', 'Cover', 'Cover'},
+	{'b 2 5', 'ja', 'Holy Circle', 'me', 'H.Crcl', 'HolyCircle'}
+}
 xivhotbar_keybinds_job['DRK'] = {
-    {'b 2 6',  'ja', 'Arcane Circle', 'me', 'Circle.'},
-    {'b 2 7',  'ja', 'Last Resort', 'me', 'Last.'},
+    {'b 2 6',  'ja', 'Arcane Circle', 'me', 'D.Circ'},
+    {'b 2 7',  'ja', 'Last Resort', 'me', 'L.Rsrt', "LastResort"},
     {'b 1 8',  'ma', 'Stun', 't', 'Stun'},
-    {'b 2 5',  'ma', 'Poisonga', 't', 'Poisonga'},
+    {'b 2 5',  'ma', 'Poisonga', 't', 'Psnga'},
     {'b 1 7',  'ja', 'Weapon Bash', 't', 'Bash'},
-    {'f 1 7',  'ma', 'Poison', 'stnpc', 'Poison'},
-    {'f 1 8',  'ma', 'Poisonga', 't', 'Poisonga'},
+    {'f 1 6',  'ma', 'Absorb-TP', 't', 'TP'},
+    {'f 1 7',  'ma', 'Poison', 'stnpc', 'Psn'},
+    {'f 1 8',  'ma', 'Poisonga', 't', 'Psnga'},
     {'f 1 9',  'ma', 'Sleep II', 'stnpc', 'Sleep'},
     {'f 1 10',  'ma', 'Bind', 'stnpc', 'Bind'},
 }
 xivhotbar_keybinds_job['SAM'] = {
 
     {'b 4 10',  'ja', 'Hasso', 'me', 'Has.', 'Hasso'},
-    {'b 3 7',  'ja', 'Meditate', 'me', 'Med.', 'Meditate'},
+    {'b 3 7',  'ja', 'Meditate', 'me', 'Med.', 'Medit.'},
     --{'b 3 8',  'ja', 'Sekkanoki', 'me', 'Sek.', 'Sekkanoki'},
 	{'b 2 8',  'ja', 'Third Eye', 'me', 'Third.', 'Third'},
 	{'b 2 7',  'ja', 'Seigan', 'me', 'Seigan', 'Seigan'},
@@ -91,48 +99,48 @@ xivhotbar_keybinds_job['SAM'] = {
 }
 
 xivhotbar_keybinds_job['BLU'] = {
-    {'b 1 7',  'ma', 'Magic Fruit', 'stpc', 'CureIV'},
-    {'b 1 8',  'ma', 'Cocoon', 'me', 'DEF+'},
+    {'b 1 7',  'ma', 'Magic Fruit', 'stpc', "Cure", 'BLU-cure'},
+    {'b 1 8',  'ma', 'Cocoon', 'me', 'DEF+', "Barrier"},
 	
-    {'b 2 5',  'ma', 'Geist Wall', 'stnpc', 'Ao.Dis'},
-	{'b 2 6', 'ma', 'Jettatura', 't', 'Fan'},
-    {'b 2 7',  'ma', 'Refueling', 'me', 'Haste'},
+    {'b 2 5',  'ma', 'Geist Wall', 'stnpc', 'Dsplga', 'Atomos'},
+	{'b 2 6', 'ma', 'Jettatura', 't', 'Fan', 'Terror2'},
+    {'b 2 7',  'ma', 'Refueling', 'me', 'Haste', 'Haste'},
 
-	{'b 4 10',  'ma', 'Healing Breeze', 'me', 'Curaga'},
+	{'b 4 10',  'ma', 'Healing Breeze', 'me', 'Curaga','CuringWaltz'},
 	
-    {'f 1 7',  'ma', 'Blank Gaze', 'stnpc', 'Dispel'},
-	{'f 1 8', 'ma', 'Sheep Song', 'stnpc', 'Ao.Zzz'},
+    {'f 1 7',  'ma', 'Blank Gaze', 'stnpc', 'Dspl', 'Eye'},
+	{'f 1 8', 'ma', 'Sheep Song', 'stnpc', 'Song', 'Sleep'},
 	
-	{'f 2 7', 'ma', 'Stinking Gas', 'stnpc', 'Ao.Gas.'},
-	{'f 2 8', 'ma', 'Frightful Roar', 'stnpc', 'Ao.DEF-'},
+	{'f 2 7', 'ma', 'Stinking Gas', 'stnpc', 'Gas'},
+	{'f 2 8', 'ma', 'Soporific', 'stnpc', 'Sleep', 'Song'},
 }
 
 xivhotbar_keybinds_job['Great Sword'] = {
 	-- Battle
-    {'b 1 1', 'weaponskill', 'Dimidiation', 'stnpc', 'Dimidiation', 'GS-Single4'},
+    {'b 1 1', 'weaponskill', 'Dimidiation', 'stnpc', 'Dimid.', 'Dimidiation'},
 	{'b 1 3', 'weaponskill', 'Spinning Slash', 't', 'Slash', 'GS-Single2'},
-	{'b 1 2', 'weaponskill', 'Resolution', 'stnpc', 'Resolution', 'GS-Single3'},
+	{'b 1 2', 'weaponskill', 'Resolution', 'stnpc', 'Reso', 'GS-Single3'},
 	--
-    {'f 1 1', 'weaponskill', 'Dimidiation', 't', 'Dimidiation', 'GS-Single4'},
-	{'f 1 2', 'weaponskill', 'Ground Strike', 't', 'G.Strike', 'GS-Single'},
-	{'f 1 3', 'weaponskill', 'Freezebite', 't', 'Freezebite', 'GS-Freeze'},
-	{'f 1 4', 'weaponskill', 'Shockwave', 't', 'Shockwave', 'GS-AoE'},
+    {'f 1 1', 'weaponskill', 'Dimidiation', 't', 'Dimid.', 'Dimidiation'},
+	{'f 1 2', 'weaponskill', 'Ground Strike', 't', 'G.Strk', 'GS-Single'},
+	{'f 1 3', 'weaponskill', 'Freezebite', 't', 'Frze', 'GS-Freeze'},
+	{'f 1 4', 'weaponskill', 'Shockwave', 't', 'Shock.', 'GS-AoE'},
 }
 xivhotbar_keybinds_job['Great Axe'] = {
 	-- Battle
-    {'b 1 1', 'weaponskill', 'Upheaval', 'stnpc', 'Upheaval', 'GA-Single4'},
-	{'b 1 2', 'weaponskill', 'Steel Cyclone', 't', 'S.Cyclone', 'GA-Single2'},
+    {'b 1 1', 'weaponskill', 'Upheaval', 'stnpc', 'Uphvl', 'GA-Single4'},
+	{'b 1 2', 'weaponskill', 'Steel Cyclone', 't', 'S.Clne', 'GA-Single2'},
 	{'b 1 3', 'weaponskill', 'Fell Cleave', 't', 'AoE', 'GAoE'},
 	{'b 1 4', 'weaponskill', 'Armor Break', 't', 'Armor', 'GA-Single3'},	
 
-    {'f 1 1', 'weaponskill', 'Upheaval', 'stnpc', 'Upheaval', 'GA-Single4'},
-	{'f 1 2', 'weaponskill', 'Steel Cyclone', 't', 'S.Cyclone', 'GA-Single2'},
+    {'f 1 1', 'weaponskill', 'Upheaval', 'stnpc', 'Uphvl', 'GA-Single4'},
+	{'f 1 2', 'weaponskill', 'Steel Cyclone', 't', 'S.Clne', 'GA-Single2'},
 	{'f 1 3', 'weaponskill', 'Fell Cleave', 't', 'AoE', 'GAoE'},
 	{'f 1 4', 'weaponskill', 'Armor Break', 't', 'Armor', 'GA-Single3'},	
 }
 xivhotbar_keybinds_job['Sword'] = {
 	-- Battle
-	{'b 1 1', 'weaponskill',  'Savage Blade', 'stnpc', 'Savage', 'Sword-Single3'}, 
+	{'b 1 1', 'weaponskill',  'Savage Blade', 'stnpc', 'Savge', 'Sword-Single3'}, 
 	{'b 1 2', 'weaponskill',  'Sanguine Blade', 't', 'Sang.', 'DBlossom'},
 	{'b 1 3', 'weaponskill',  'Requiescat', 't', 'Requi.', 'Sword-Requiescat'},
 }
@@ -143,7 +151,27 @@ xivhotbar_keybinds_job['Club'] = {
 }
 xivhotbar_keybinds_job['Axe'] = {
 	-- Battle
-	{'b 1 2', 'weaponskill', 'Rampage', 't', 'Rampage', 'Axe-Single2'},
-	{'b 1 1', 'weaponskill', 'Decimation', 'stnpc', 'Decimate', 'Decimation'},
+	{'b 1 2', 'weaponskill', 'Rampage', 't', 'Rampge', 'Axe-Single2'},
+	{'b 1 1', 'weaponskill', 'Decimation', 'stnpc', 'Dcimte', 'Decimation'},
+}
+xivhotbar_keybinds_job['SCH'] = {
+    {'b 2 5',  'ja', 'Addendum: White', 'me', 'Add.W'},
+    {'b 2 7',  'ja', 'Light Arts', 'me', 'Light'},
+    {'b 2 6',  'ja', 'Dark Arts', 'me', 'Dark'},
+    {'b 1 8',  'ja', 'Accession', 'me', 'Aoe'},
+    {'b 1 7',  'ja', 'Sublimation', 'me', 'Sub.'},
+	{'f 2 10', 'ma', 'Klimaform', 'me', 'Klima'},
+
+ 	{'f 4 1', 'ma', 'Cure III', 'stpc', 'CureIII'},
+ 	{'f 3 1', 'ma', 'Cure IV', 'stpc', 'CureIV'},
+	{'f 3 2', 'ma', 'Hailstorm', 'me', 'Hail'},
+	{'f 3 3', 'ma', 'Voidstorm', 'stpc', 'Void'},
+	{'f 3 4', 'ma', 'Firestorm', 'stpc', 'Fire'},
+	{'f 3 5', 'ma', 'Sandstorm', 'stpc', 'Sand'},
+	{'f 3 6', 'ma', 'Windstorm', 'stpc', 'Wind'},
+	{'f 3 7', 'ma', 'Thunderstorm', 'stpc', 'Thndr'},
+	{'f 3 8', 'ma', 'Aurorastorm', 'stpc', 'Aurora'},
+	{'f 4 9', 'ma', 'Reraise', 'me', 'Reraise'},
+
 }
 return xivhotbar_keybinds_job
