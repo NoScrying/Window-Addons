2e6d02f3b4dedc48fd765be944feef121b58ef59
NoScrying <42573630+NoScrying@users.noreply.github.com>
2024-11-09 02:30:06 +0100

    Update DNC.lua

--- xivhotbar/data/Noscrying/DNC.lua
+++ xivhotbar/data/Noscrying/DNC.lua
@@ -1,7 +1,7 @@
 
 xivhotbar_keybinds_job['Base'] = {
  -- Hotbar #1
-	{'b 1 5', 'ja', 'Animated Flourish', 'stnpc', 'Provoke'},
+	{'b 1 5', 'ja', 'Animated Flourish', 'stnpc', 'Provoke',"tile150"},
 	{'b 1 8', 'ja', 'Striking Flourish', 'me', 'DA+1', "Striking"},
 	{'b 1 7', 'ja', 'Climactic Flourish', 'me', 'Crit+1', 'Ternary'},
 	{'b 1 6', 'input', "//cancel Saber Dance,Fan Dance", '', 'DA Off', "SaberDanceOff"},
@@ -20,11 +20,11 @@ xivhotbar_keybinds_job['Base'] = {
 	{'b 3 4', 'ja', 'Divine Waltz', 'stpc', 'Cura', 'Divine'},
 	{'b 3 5', 'ja', 'Divine Waltz II', 'stpc', 'Curaga', 'Divine2'},
 	{'b 3 6', 'ja', 'Healing Waltz', 'stpc', 'Esuna', 'HealingWaltz'},
-	{'b 3 7', 'ja', 'Reverse Flourish', 'me', 'Rvrs', 'Reverse'},
+	{'b 3 7', 'ja', 'Reverse Flourish', 'me', 'Rvrse', 'Reverse'},
 	{'b 3 8', 'ja', 'Building Flourish', 'me', 'WS+', 'BuildingFlourish'},
 	
-	{'f 3 9', 'ja', 'Trance', 'me', 'TP+',},
-	{'f 3 10', 'ja', 'Grand Pas', 'me', 'Flourish+',},
+	{'f 3 9', 'ja', 'Trance', 'me', 'TP+',"tile972"},
+	{'f 3 10', 'ja', 'Grand Pas', 'me', 'Flourish+',"tile711"},
  -- Hotbar #4
     {'b 2 1', 'ja', 'Drain Samba III', 'me', 'Drain', 'Drain'},
     {'b 2 2', 'ja', 'Haste Samba', 'me', 'Haste', 'HasteSamba'},
@@ -41,21 +41,22 @@ xivhotbar_keybinds_job['Base'] = {
 xivhotbar_keybinds_job['Dagger'] = {
 	-- Battle
 	{'b 1 1', 'weaponskill', "Rudra's Storm", 'stnpc', 'Rudra', 'Zesho'},
-    {'b 1 2', 'weaponskill', 'Pyrrhic Kleos', 't', 'Pyrrhic.', 'Dagger-Single2'},
-    {'b 1 3', 'weaponskill', 'Evisceration', 't', 'Evisc.', 'Dagger-Multi'},
+    {'b 1 2', 'weaponskill', 'Pyrrhic Kleos', 'stnpc', 'Pyrrhic.', 'Dagger-Single2'},
+    {'b 1 3', 'weaponskill', 'Evisceration', 'stnpc', 'Evisc.', 'Dagger-Multi'},
 	{'b 1 4', 'weaponskill', 'Aeolian Edge', 't', 'Aeolian.', 'Dagger-AoE'},
-    {'b 2 4', 'weaponskill', 'Shark Bite', 't', 'Shark.', "Dagger-Single2"},
-    {'b 2 5', 'weaponskill', 'Exenterator', 't', 'Exent.', "Dagger-Single4"},
+    {'b 2 4', 'weaponskill', 'Shark Bite', 't', 'Shark.', "Sharkbite"},
+    {'b 2 5', 'weaponskill', 'Exenterator', 't', 'Exent.', "Exenterator"},
+    {'b 2 6', 'weaponskill', 'Dancing Edge', 't', 'Dance.', "Dancing Edge"},
 }
 xivhotbar_keybinds_job['Hand-to-hand'] = {
-{'b 1 1', 'weaponskill', 'Tornado Kick', 't', 'T.Kick', 'H2H-Kick'},
+{'b 1 1', 'weaponskill', 'Tornado Kick', 'stnpc', 'T.Kick', 'H2H-Kick'},
 {'b 1 4', 'weaponskill', 'Spinning Attack', 't', 'AoE', 'H2H-AoE'},
-{'b 1 3', 'weaponskill', 'Asuran Fists', 't', 'Asura', 'H2H-Multi'},
-{'b 1 2', 'weaponskill', 'Raging Fists', 't', 'Rage', 'H2H-Single4'},
+{'b 1 3', 'weaponskill', 'Asuran Fists', 'stnpc', 'Asura', 'H2H-Multi'},
+{'b 1 2', 'weaponskill', 'Raging Fists', 'stnpc', 'Rage', 'H2H-Single4'},
 }
 
 xivhotbar_keybinds_job['WAR'] = {
-    {'b 1 5',  'ja', 'Provoke', 'stnpc', 'Prov.'},
+    {'b 1 5',  'ja', 'Provoke', 'stnpc', 'Prov.',"tile150"},
     {'b 2 7',  'ja', 'Berserk', 'me', 'Brsrk', 'Berserk'},
     {'b 2 8',  'ja', 'Warcry', 'me', 'War.', 'Warcry'},
 	{'b 2 9', 'ja', 'Aggressor', 'me', 'Aggr.', 'Agressor'},
@@ -99,5 +100,83 @@ xivhotbar_keybinds_job['COR'] = {
 	{'f 1 6', 'ja', 'Evoker\'s Roll', 'me', 'Refresh', 'Roll5'},
 	{'f 1 7', 'ja', 'Corsair\'s Roll', 'me', 'EXP', 'Roll5'},
 	{'f 1 10', 'ja', 'Double-Up', 'me', 'Double'},
+}
+xivhotbar_keybinds_job['RDM'] = {
+	{'f 1 9', 'ma', 'Phalanx', 'me', 'Phalnx', "Phalanx"},
+	{'f 1 8', 'ma', 'Dispel', 't', 'Dspel','tile877'},
+	
+	{'f 4 1', 'ma', 'Stone II', 't', 'Stone',"tile031"},
+	{'f 4 2', 'ma', 'Water II', 't', 'Water',"tile085"},
+	{'f 4 3', 'ma', 'Aero II', 't', 'Aero',"tile056"},
+	{'f 4 4', 'ma', 'Fire II', 't', 'Fire',"tile054"},
+	{'f 4 5', 'ma', 'Blizzard II', 't', 'Bliz.',"tile053"},
+	{'f 4 6', 'ma', 'Thunder', 't', 'Thund.',"tile055"},
+	
+	{'f 1 10', 'ja', 'Convert', 'me', 'Convert',"tile813"},
+	{'f 2 3', 'ma', 'Refresh', 'stpc', 'Refresh',"tile617"},
+	{'f 2 4', 'ma', 'Haste', 'stpc', 'Haste',"tile860"},
+	{'f 2 1', 'ma', 'Diaga', 't', 'Diaga'},
+	{'f 2 2', 'ma', 'Dia', 't', 'Dia',"tile455"},
+	{'f 2 7', 'ma', 'Stoneskin', 'me', 'StnSkn',"tile639"},
+	{'f 2 8', 'ma', 'Enthunder', 'me',"En-Ele","tile828"},
+	{'f 2 9', 'ma', 'Ice Spikes', 'me', 'I.Spks',"tile385"},
+
+	{'f 3 1', 'ma', 'Protect III', 'me', 'Prtct.',"tile462"},
+	{'f 3 2', 'ma', 'Shell II', 'me', 'Shell',"tile497"},
+	{'f 3 3', 'ma', 'Aquaveil', 'me', 'Aqua',"tile630"},	
+	{'f 3 7', 'ma', 'Barparalyze', 'me', 'B.Par',"tile821"},		
+	{'f 3 8', 'ma', 'Barblizzard', 'me', 'B.Ice',"tile822"},	
+	{'f 4 9', 'ma', 'Sneak', 'stpc', 'Sneak',"tile019"},		
+	{'f 4 10', 'ma', 'Invisible', 'stpc', 'Invis',"tile086"},	
+}
+xivhotbar_keybinds_job['BLM'] = {
+	{'f 4 10', 'ja', 'Elemental Seal', 'me', 'E.Seal'},
+	{'f 4 8', 'ma', 'Warp', 'me', 'Warp'},
+ 	{'f 4 9', 'ma', 'Warp II', 'stpc', 'WarpII'},
+ 	{'f 2 10', 'ma', 'Escape', 'me', 'Esc'},
+ 	{'f 4 4', 'ma', 'Sleepga', 't', 'Slpga'},
+ 	{'f 4 1', 'ma', 'Stone III', 't', 'Stone'},
+ 	{'f 4 2', 'ma', 'Firaga II', 't', 'Firga'},
+ 	{'f 4 3', 'ma', 'Burst', 't', 'Burst'},
+	{'f 2 9', 'ma', 'Ice Spikes', 'me', 'Ice'},
+	{'f 2 1', 'ma', 'Poisonga', 't', 'Psnga', "Spider_Venom"},
+	{'f 2 3', 'ma', 'Poison II', 't', 'Psn'},	
+	{'f 3 2', 'ma', 'Choke', 't', 'Choke'},	
+	{'f 3 4', 'ma', 'Bio II', 't', 'Bio'},	
+	{'f 3 3', 'ma', 'Burn', 't', 'Burn'},	
+	{'f 3 1', 'ma', 'Shock', 't', 'Shock'},	
+	{'f 1 10', 'ma', 'Stun', 't', 'Stun',"tile959"},		
+	{'f 1 8', 'ma', 'Sleep II', 't', 'SlpII'},	
+	{'f 1 9', 'ma', 'Sleepga II', 't', 'SlpgaII'},		
+}
+
+xivhotbar_keybinds_job['RUN'] = {
+	{'f 3 1', 'ja', 'Ignis', 'me', 'Ice', 'ignis-icon'},
+    {'f 3 2', 'ja', 'Gelus', 'me', 'Wind', 'gelus-icon'},
+    {'f 3 3', 'ja', 'Flabra', 'me', 'Earth', 'flabra-icon'},
+    {'f 3 4', 'ja', 'Tellus', 'me', 'Thun.', 'tellus-icon'},
+    {'f 3 5', 'ja', 'Sulpor', 'me', 'Water', 'sulpor-icon'},
+    {'f 3 6', 'ja', 'Unda', 'me', 'Fire', 'unda-icon'},
+    {'f 3 7', 'ja', 'Lux', 'me', 'Dark', 'lux-icon'},
+    {'f 3 8', 'ja', 'Tenebrae', 'me', 'Light', 'tenebrae-icon'},
+	
+
+	{'f 4 1', 'ja', 'Vallation', 'me', 'Val.', 'Vallation'},
+	{'f 4 2', 'ja', 'Valiance', 'me', 'Valnce', 'Valiance'},
+	{'f 4 3', 'ja', 'Pflug', 'me', 'Pflug', 'Oblation'},
+	{'f 4 4', 'ja', 'Swordplay', 'me', 'Sword.', 'Swordplay'},
+	
+	{'f 1 7', 'ma', 'Aquaveil', 'me', 'Veil',"Aquaveil"},
+	{'f 1 8', 'ma', 'Flash', 't', 'Flash',"Flash"},	
+	{'f 1 9', 'ma', 'Stoneskin', 'me', 'StnSkn',"tile639"},	
+}
+xivhotbar_keybinds_job['WHM'] = {
+	{'f 4 1', 'ma', 'Teleport-Dem', 'me', 'Dem'},	
+	{'f 4 2', 'ma', 'Teleport-Mea', 'me', 'Mea'},	
+	{'f 4 3', 'ma', 'Teleport-Holla', 'me', 'Holla'},	
+	{'f 4 4', 'ma', 'Teleport-Vahzl', 'me', 'Vahzl'},	
+	{'f 3 1', 'ma', 'Recall-Pashh', 'me', 'Pash'},	
+	{'f 3 2', 'ma', 'Recall-Jugner', 'me', 'Jugn'},	
+	{'f 3 3', 'ma', 'Recall-Meriph', 'me', 'Meriph'},	
 }
 return xivhotbar_keybinds_job
