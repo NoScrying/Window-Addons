2dccfd1373106315aa0708256d33632f86de5e90
NoScrying <42573630+NoScrying@users.noreply.github.com>
2024-11-09 02:29:42 +0100

    Update BRD.lua

--- xivhotbar/data/Noscrying/BRD.lua
+++ xivhotbar/data/Noscrying/BRD.lua
@@ -1,47 +1,46 @@
-
 xivhotbar_keybinds_job['Base'] = {
  -- Hotbar #1
-	{'b 1 5', 'ma',  'Dragonfoe Mambo', 'stpc', 'Eva 2'},
- 	{'b 1 6', 'ma',  'Sheepfoe Mambo', 'stpc', 'Eva 1'},
- 	{'b 1 7', 'ja',  'Marcato', 'me', 'Marcto'},
- 	{'b 1 8', 'ma',  "Army's Paeon", 'stpc', 'Dupe'},	
- 	{'b 1 9', 'ma',  "Army's Paeon II", 'stpc', 'Dupe'},	
- 	{'b 1 10', 'ma',  "Goddess's Hymnus", 'stpc', 'RR'},	
+	{'b 1 5', 'ma',  'Dragonfoe Mambo', 'stpc', 'Eva 2','Misty_Step2'},
+ 	{'b 1 6', 'ma',  'Sheepfoe Mambo', 'stpc', 'Eva 1','Misty_Step2'},
+ 	{'b 1 7', 'ja',  'Marcato', 'me', 'Marcto',"tile528"},
+ 	{'b 1 8', 'ma',  "Army's Paeon", 'stpc', 'Dupe','tile078'},	
+ 	{'b 1 9', 'ma',  "Army's Paeon II", 'stpc', 'Dupe','tile078'},	
+ 	{'b 1 10', 'ma',  "Goddess's Hymnus", 'stpc', 'RR','tile800'},	
 
  -- Hotbar #2
-	{'b 2 1', 'ma',  'Blade Madrigal', 'stpc', 'Ac 2'},
-	{'b 2 2', 'ma',  'Sword Madrigal', 'stpc', 'Ac 1'},
-	{'b 2 3', 'ma',  'Mage\'s Ballad III', 'stpc', 'MP'},
-	{'b 2 4', 'ma',  'Mage\'s Ballad II', 'stpc', 'MP'},
-	{'b 2 5', 'ma',  'Army\'s Paeon VI', 'stpc', 'HP'},
-	{'b 2 6', 'ma',  'Sentinel\'s Scherzo', 'stpc', 'Srzo'},
-	{'b 2 10', 'ma',  'Chocobo Mazurka', 'me', 'Run'},
-	{'b 2 7', 'ja',  'Troubadour', 'me', 'Troub.'},
-	{'b 2 8', 'ja',  'Nightingale', 'me', 'Night.'},
-	{'b 2 9', 'ja',  'Pianissimo', 'me', 'Pian.'},
+	{'b 2 1', 'ma',  'Blade Madrigal', 'stpc', 'Ac 2','tile008'},
+	{'b 2 2', 'ma',  'Sword Madrigal', 'stpc', 'Ac 1','tile008'},
+	{'b 2 3', 'ma',  'Mage\'s Ballad III', 'stpc', 'MP 3','tile617'},
+	{'b 2 4', 'ma',  'Mage\'s Ballad II', 'stpc', 'MP 2','tile617'},
+	{'b 2 5', 'ma',  'Army\'s Paeon VI', 'stpc', 'HP',"tile792"},
+	{'b 2 6', 'ma',  'Sentinel\'s Scherzo', 'stpc', 'Srzo','tile067'},
+	{'b 2 10', 'ma',  'Chocobo Mazurka', 'me', 'Run','tile090'},
+	{'b 2 7', 'ja',  'Troubadour', 'me', 'Troub.','tile761'},
+	{'b 2 8', 'ja',  'Nightingale', 'me', 'Night.','tile856'},
+	{'b 2 9', 'ja',  'Pianissimo', 'me', 'Pian.','tile753'},
  -- Hotbar #3
-	{'b 3 1', 'ma',  'Magic Finale', 'stnpc', 'Dspl'},
-	{'b 3 2', 'ma',  'Carnage Elegy', 'stnpc', 'Slow'},
-	{'b 3 3', 'ma',  'Foe Requiem VII', 'stnpc', 'Req.'},
-	{'b 3 4', 'ma',  'Pining Nocturne', 'stnpc', '-M.Acc'},
- 	{'b 3 5', 'ma',  'Archer\'s Prelude', 'stpc', 'RAC 2'},
- 	{'b 3 6', 'ma',  'Hunter\'s Prelude', 'stpc', 'RAC 1'},
-	{'b 3 7', 'ja',  'Tenuto', 'me', 'Tenuto'},
-	{'b 3 8', 'ma',  'Mage\'s Ballad', 'stpc', 'MP'},
-	{'b 3 9', 'ma',  'Valor Minuet III', 'stpc', 'ATK'},
-	{'b 3 10', 'ma',  'Herculean Etude', 'stpc', 'STR+'},
+	{'b 3 1', 'ma',  'Magic Finale', 'stnpc', 'Dspl','tile877'},
+	{'b 3 2', 'ma',  'Carnage Elegy', 'stnpc', 'Slow','tile075'},
+	{'b 3 3', 'ma',  'Foe Requiem VII', 'stnpc', 'Req.',"tile700"},
+	{'b 3 4', 'ma',  'Pining Nocturne', 'stnpc', '-M.Acc',"tile164"},
+ 	{'b 3 5', 'ma',  'Archer\'s Prelude', 'stpc', 'RAC 2', "tile010"},
+ 	{'b 3 6', 'ma',  'Hunter\'s Prelude', 'stpc', 'RAC 1', "tile010"},
+	{'b 3 7', 'ja',  'Tenuto', 'me', 'Tenuto',"Generic_Feature_Icon"},
+	{'b 3 8', 'ma',  'Mage\'s Ballad', 'stpc', 'MP 1',"tile617",},
+	{'b 3 9', 'ma',  'Valor Minuet III', 'stpc', 'ATK III','Extra_Attack2'},
+	{'b 3 10', 'ma',  'Herculean Etude', 'stpc', 'STR+','tile414'},
  -- Hotbar #4
-	{'b 4 2', 'ma',  'Valor Minuet V', 'stpc', 'ATK V'},
-	{'b 4 1', 'ma',  'Valor Minuet IV', 'stpc', 'ATK IV'},
-	{'b 4 3', 'ma',  'Honor March', 'stpc', 'Honor'},
-	{'b 4 4', 'ma',  'Victory March', 'stpc', 'HST 2'},
+	{'b 4 2', 'ma',  'Valor Minuet V', 'stpc', 'ATK V','Extra_Attack2'},
+	{'b 4 1', 'ma',  'Valor Minuet IV', 'stpc', 'ATK IV','Extra_Attack2'},
+	{'b 4 3', 'ma',  'Honor March', 'stpc', 'Honor','Heroism2'},
+	{'b 4 4', 'ma',  'Victory March', 'stpc', 'HST 2','tile860'},
 
- 	{'b 4 5', 'ma',  'Knight\'s Minne V', 'stpc', 'Min'},	
- 	{'b 4 6', 'ma',  'Knight\'s Minne IV', 'stpc', 'Min'},	
-	{'b 4 7', 'ma',  'Horde Lullaby', 'stnpc', 'Slpga'},	
-	{'b 4 8', 'ma',  'Horde Lullaby II', 'stnpc', 'SlpgaII'},
-	{'b 4 9', 'ma',  'Foe Lullaby II', 'stnpc', 'SlpII'},		
-	{'b 4 10', 'ma',  "Maiden's Virelai", 'stnpc', 'Charm'},		
+ 	{'b 4 5', 'ma',  'Knight\'s Minne V', 'stpc', 'Min IV','tile048'},	
+ 	{'b 4 6', 'ma',  'Knight\'s Minne IV', 'stpc', 'Min V','tile048'},	
+	{'b 4 7', 'ma',  'Horde Lullaby', 'stnpc', 'Slpga','tile448'},	
+	{'b 4 8', 'ma',  'Horde Lullaby II', 'stnpc', 'SlpgaII','tile250'},
+	{'b 4 9', 'ma',  'Foe Lullaby II', 'stnpc', 'SlpII','tile944'},		
+	{'b 4 10', 'ma',  "Maiden's Virelai", 'stnpc', 'Charm','tile299'},		
 	-- {'f 2 1', 'ma',  'Fire Threnody', 'stnpc', 'Fire-'},
 	-- {'f 2 2', 'ma',  'Ice Threnody', 'stnpc', 'Ice-'},
 	-- {'f 2 3', 'ma',  'Wind Threnody', 'stnpc', 'Wind-'},
@@ -54,8 +53,8 @@ xivhotbar_keybinds_job['Base'] = {
 	--{'f 2 9', 'ma',  'Foe Lullaby', 'stnpc', 'Sleep'},
 	--{'f 2 10', 'ma',  'Foe Lullaby II', 'stnpc', 'SleepII'},
 	
-	{'f 3 9', 'ja',  'Clarion Call', 'me', 'C.Call'},
-	{'f 3 10', 'ja',  'Soul Voice', 'me', 'S.Voice'},
+	{'f 3 9', 'ja',  'Clarion Call', 'me', 'C.Call','tile1003'},
+	{'f 3 10', 'ja',  'Soul Voice', 'me', 'S.Voice','tile684'},
 
 	{'b 6 1', 'ma',  'Light Carol', 'stpc', 'Light'},
 	{'b 6 2', 'ma',  'Dark Carol', 'stpc', 'Dark'},
@@ -72,10 +71,10 @@ xivhotbar_keybinds_job['Base'] = {
 
 xivhotbar_keybinds_job['Sword'] = {
 	-- Battle
-	{'b 1 1', 'weaponskill',  'Savage Blade', 'stnpc', 'Savge', 'Sword-Single3'}, 
+	{'b 1 1', 'weaponskill',  'Savage Blade', 'stnpc', 'Savge', 'Imperator'}, 
 	{'b 1 2', 'weaponskill',  'Circle Blade', 't', 'AoE', 'Sword-AoE'}, 
 	
-	{'f 1 1', 'weaponskill',  'Savage Blade', 'stnpc', 'Savge', 'Sword-Single3'}, 
+	{'f 1 1', 'weaponskill',  'Savage Blade', 'stnpc', 'Savge', 'Imperator'}, 
 	{'f 1 2', 'weaponskill',  'Circle Blade', 't', 'AoE', 'Sword-AoE'}, 
 }
 xivhotbar_keybinds_job['Dagger'] = {
@@ -93,8 +92,8 @@ xivhotbar_keybinds_job['Dagger'] = {
 
 xivhotbar_keybinds_job['PLD'] = {
     {'f 1 7',  'ma', 'Cure IV', 'stpc', 'CureIV'},
-    {'f 1 8',  'ma', 'Banishga', 't', 'Bnshga', "Angel"},
-    {'f 1 9',  'ma', 'Flash', 'stnpc', 'Flash', "Flash"},
+    {'f 1 8',  'ma', 'Banishga', 't', 'Bnshga', 'Angel'},
+    {'f 1 9',  'ma', 'Flash', 'stnpc', 'Flash', 'Flash'},
     {'f 2 7',  'ja', 'Sentinel', 'me', 'Sntnl', 'Sentinel'},
     {'f 2 8',  'ja', 'Cover', 'stpc', 'Cver', 'Cover'},
 	{'f 1 5', 'ja', 'Holy Circle', 'me', 'H.Crcl', 'HolyCircle'}
@@ -107,7 +106,7 @@ xivhotbar_keybinds_job['DNC'] = {
 	{'f 3 3', 'ja', 'Healing Waltz', 'stpc', 'Esuna', 'HealingWaltz'},
 	
 	{'f 4 10', 'ja', 'Contradance', 'me', 'Cure+', 'Halation'},
-    {'f 2 2', 'ja', 'Haste Samba', 'me', 'Haste', 'Haste'},
+    {'f 2 2', 'ja', 'Haste Samba', 'me', 'Haste', 'HasteSamba'},
 
 
 	{'f 4 1', 'ja', 'Box Step', 't', 'Box', 'Box'},
@@ -153,20 +152,28 @@ xivhotbar_keybinds_job['WHM'] = {
 	{'f 2 1', 'ma', 'Banish II', 'stnpc', 'Bnsh'},		
 	{'f 2 2', 'ma', 'Banishga II', 'stnpc', 'Bnshga'},	
 	{'f 2 3', 'ma', 'Silence', 'stnpc', 'Slnce'},		
-	{'f 2 4', 'ma', 'Stoneskin', 'me', 'S.Skin'},	
-	{'f 2 5', 'ma', 'Aquaveil', 'me', 'Veil'},		
+	{'f 2 4', 'ma', 'Stoneskin', 'me', 'StnSkn',"tile639"},
+	{'f 2 5', 'ma', 'Aquaveil', 'me', 'Veil',"tile630"},		
 	{'f 2 6', 'ma', 'Blink', 'me', 'Blnk'},				
 	
 }
 xivhotbar_keybinds_job['BLM'] = {
+	{'b 4 10', 'ja', 'Elemental Seal', 'me', 'E.Seal'},
 	{'f 4 10', 'ja', 'Elemental Seal', 'me', 'E.Seal'},
-	{'f 4 1', 'ma', 'Warp', 'me', 'Warp'},
- 	{'f 4 2', 'ma', 'Warp II', 'stpc', 'WarpII'},
- 	{'f 4 3', 'ma', 'Retrace', 'stpc', 'Rtrce'},	
- 	{'f 4 4', 'ma', 'Tractor', 'stpc', 'Trctr'},	
- 	{'f 1 7', 'ma', 'Stun', 't', 'Stun'},
- 	{'f 1 9', 'ma', 'Sleepga', 't', 'Slpga'},
-	{'f 1 10', 'ma', 'Ice Spikes', 'me', 'Ice'},
+	{'f 4 8', 'ma', 'Warp', 'me', 'Warp'},
+ 	{'f 4 9', 'ma', 'Warp II', 'stpc', 'WarpII'},
+ 	{'f 2 10', 'ma', 'Escape', 'me', 'Esc'},
+ 	{'f 4 4', 'ma', 'Sleepga', 't', 'Slpga'},
+ 	{'f 4 1', 'ma', 'Stone III', 't', 'Stone'},
+ 	{'f 4 2', 'ma', 'Stonega II', 't', 'Stnga'},
+	{'f 2 9', 'ma', 'Ice Spikes', 'me', 'Ice'},
+	{'f 2 1', 'ma', 'Poisonga', 't', 'Psnga', "Spider_Venom"},
+	{'f 2 3', 'ma', 'Poison II', 't', 'Psn'},	
+	{'f 3 2', 'ma', 'Choke', 't', 'Choke'},	
+	{'f 3 4', 'ma', 'Bio II', 't', 'Bio'},	
+	{'f 3 3', 'ma', 'Burn', 't', 'Burn'},	
+	{'f 3 1', 'ma', 'Shock', 't', 'Shock'},	
+	{'f 1 10', 'ma', 'Stun', 't', 'Stun',"tile959"},		
 }
 xivhotbar_keybinds_job['RDM'] = {
 	--{'b 1 9', 'ma', 'Phalanx', 'me', 'Phalanx', 'Stoneskin'},
@@ -181,7 +188,7 @@ xivhotbar_keybinds_job['RDM'] = {
 	{'f 3 3', 'ma', 'Refresh', 'stpc', 'Rfresh'},
 	{'f 2 4', 'ma', 'Haste', 'stpc', 'Haste'},
 	{'f 2 1', 'ma', 'Dia II', 't', 'Dia'},
-	{'f 2 7', 'ma', 'Stoneskin', 'me', 'StnSkn'},
+	{'f 2 7', 'ma', 'Stoneskin', 'me', 'StnSkn',"tile639"},
 	{'f 2 8', 'ma', 'Enfire', 'me', 'En-Ele'},
 	{'f 1 9', 'ma', 'Phalanx', 'me', 'Phalnx'},
 
